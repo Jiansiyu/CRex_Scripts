1fca954f3aa6626439391c1877cfa8849c9e954e
Siyu <jiansiyu@gmail.com>
2020-08-24 04:07:43 -0400

    add standalone poiting check script

---
+++ Tools/PlotCut/PointingCheck/pointingCheck.C
@@ -0,0 +1,35 @@
+/*
+ * pointingCheck.C
+ *
+ * Used for generate the plot for Pionting Measurement
+ *
+ *  Created on: Aug 24, 2020
+ *      Author: Siyu Jian
+ *              jiansiyu@gmail.com
+ */
+
+#include <TString.h>
+#include <TH1.h>
+#include <TCanvas.h>
+void DataLoader(TString fname){
+
+}
+
+
+void PlotPointingL(){
+
+
+
+}
+
+
+void PlotPointingR(){
+
+
+
+}
+
+void PlotPointing(){
+
+}
+
